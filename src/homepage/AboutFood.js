cfc34e40479415bee92da608b894ff09ecb4103d
Ashwin <mahesh.ashwin1998@gmail.com>
2019-11-05 00:34:34 -0500

    Fixed styling for second food

--- src/homepage/AboutFood.js
+++ src/homepage/AboutFood.js
@@ -5,20 +5,31 @@ import ribsImage from './images/ribs.jpg'
 const useStyles = makeStyles(theme => ({
   sectionWrapper:{
     backgroundColor: 'white',
-    display: 'flex',
-    justifyContent: 'center',
-    width: '900px',
+    textAlign: 'center',
+    paddingTop: '30px',
+    paddingBottom: '30px'
+    // display: 'flex',
+    // justifyContent: 'center',
   },
   imageStyle:{
     width: '200px',
     height: '200px',
     borderRadius: '100px',
   },
+  firstAbout: {
+    // marginTop: '30px',
+    display: 'block',
+    height: '200px',
+  },
+  secondAbout: {
+    marginTop: '60px',
+    display: 'block',
+    height: '200px',
+  },
   singleAbout: {
     marginTop: '30px',
     display: 'block',
-    border: '1px solid red',
-    height: '200px'
+    height: '200px',
   },
   textDiv: {
     marginLeft: '20px',
@@ -28,40 +39,45 @@ const useStyles = makeStyles(theme => ({
   },
   bigText: {
     fontWeight: 'bold',
-    fontSize: '18pt',
-    // display: 'inline-block',    
-    margin: '0px'
+    fontSize: '18pt', 
+    margin: '0px',
+    textAlign: 'left'
   },
   smallText: {
-    // display: 'inline-block',
     fontSize: '15pt',
     margin: '0px',
-    marginTop: '20px'
+    marginTop: '20px',
+    textAlign: 'left'
   },
+  block: {display: 'block'},
 }));
 
 function AboutFood() {
   const styles = useStyles();
 
   function renderFirstAbout(){
+    const bigText="Smoked Memphis BBQ"
+    const smallText = "Ribs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum IpsumRibs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum Ipsum";
     return (
-      <div className={styles.singleAbout}>
+      <div className={styles.firstAbout}>
         <img className={styles.imageStyle} src={ribsImage}></img>
         <div className={styles.textDiv}>
-          <p className={styles.bigText}>Smoked Memphis BBQ</p>
-          <p className={styles.smallText}>Ribs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum IpsumRibs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum Ipsum</p>
+          <p className={styles.bigText}>{bigText}</p>
+          <p className={styles.smallText}>{smallText}</p>
         </div>
       </div>
     );
   }
 
   function renderSecondAbout(){
+    const bigText = "Unique Hot Dogs and Sausages";
+    const smallText = "Most Unique Hot Dogs in Memphis! We serve hot dogs and smoked sausages topped with homemade ground beef and coleslaw. Will add more to this section + reword in the future. Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum"
     return (
-      <div className={styles.singleAbout}>
+      <div className={styles.secondAbout}>
         <img className={styles.imageStyle} src={ribsImage}></img>
         <div className={styles.textDiv}>
-          <p className={styles.bigText}>Smoked Memphis BBQ</p>
-          <p className={styles.smallText}>Ribs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum IpsumRibs and such that will make your mouth salivate. TBD Will rewrite this section. Lorum Ipsum</p>
+          <p className={styles.bigText}>{bigText}</p>
+          <p className={styles.smallText}>{smallText}</p>
         </div>
       </div>
     )
@@ -70,7 +86,7 @@ function AboutFood() {
   return(
     <div className={styles.sectionWrapper}>
       {renderFirstAbout()}
-      {/* {renderSecondAbout()} */}
+      {renderSecondAbout()}
     </div>
   );
 }
