388f91b72a1f894676d176f3d63af4794dd47580
Ashwin <mahesh.ashwin1998@gmail.com>
2019-11-17 21:43:10 -0500

    Working on changes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const expressSession = require('express-session');
 
 const pino = require('express-pino-logger')();
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(expressSession({
+  secret:'YouWillNotGuessThis',
+  resave:true,
+  saveUninitialized:true,
+  cookie:{maxAge:1*24*60*60*1000}
+}));
 app.use(pino)
 
 app.get('/', (request, response) => {
@@ -17,7 +23,7 @@ app.get('/login', (request, response) => {
   return response.json({success: 1, message: 'The test is successful'})
 })
 
-app.listen(8070, () => {
-  console.log("Server is listening on Port 8070")
+app.listen(8000, () => {
+  console.log("Server is listening on Port 8000")
 })
 
