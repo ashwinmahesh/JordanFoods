43e0843451a77fc230c2ee917a16dc6cc128f8bc
Ashwin <mahesh.ashwin1998@gmail.com>
2019-11-18 18:46:20 -0500

    Before adding inner repository

--- client/src/about/about.js
+++ client/src/about/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import headshot1 from './images/headshot.jpg'
+import headshot1 from './images/headshot.jpg';
+import headshot2 from './images/headshot2.jpg';
 
 const useStyles = makeStyles(theme => ({
   pageWrapper: {
@@ -21,17 +22,30 @@ const useStyles = makeStyles(theme => ({
   },
   headshot: {
     width: '300px',
-    borderRadius: '150px'
+    borderRadius: '200px'
   },
   bioWrapper: {
     width: '375px',
-    border: '1px solid red',
     textAlign: 'center',
-    display:'inline-block'
+    display:'inline-block',
+    padding: '15px'
   },
   bothBios: {
     display: 'flex',
     justifyContent: 'space-evenly'
+  },
+  nameText: {
+    fontSize: '18pt',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: '0px',
+    marginTop: '15px'
+  },
+  description: {
+    fontSize: '14pt',
+    margin: '0px',
+    marginTop: '15px',
+    textAlign: 'justify'
   }
 }));
 
@@ -42,6 +56,22 @@ function About() {
     return (
       <div className={styles.bioWrapper}>
         <img className={styles.headshot} src={headshot1} alt='Headshot 1'/>
+        <p className={styles.nameText}>Dwight Jordan</p>
+        <p className={styles.description}>
+          Born and raised in Memphis, Dwight is a big fan of the culture and of Beale Street. Dwight is passionate about making his business, and has been doing it for a long time. His other interests include watching the Tennessee Titans, playing the guitar, and going Hiking
+        </p>
+      </div>
+    )
+  }
+
+  function renderBio2() {
+    return (
+      <div className={styles.bioWrapper}>
+        <img className={styles.headshot} src={headshot2} alt='Headshot 2'/>
+        <p className={styles.nameText}>Mrs. Jordan</p>
+        <p className={styles.description}>
+          Born and raised in Memphis, Dwight is a big fan of the culture and of Beale Street. Dwight is passionate about making his business, and has been doing it for a long time. His other interests include watching the Tennessee Titans, playing the guitar, and going Hiking
+        </p>
       </div>
     )
   }
@@ -49,12 +79,12 @@ function About() {
   return(
     <div className={styles.pageWrapper}>
       <p className={styles.pageTitle}>Meet the Owners</p>
-      <p className={styles.subtext}>
+      {/* <p className={styles.subtext}>
         Jordan's Hot & Cold Vending is a local, family-owned business. Our goal is to bring you tasty, Memphis cooking to satisy your late-night hunger.
-      </p>
+      </p> */}
       <div className={styles.bothBios}>
         {renderBio1()}
-        {renderBio1()}
+        {renderBio2()}
       </div>
     </div>
   )
